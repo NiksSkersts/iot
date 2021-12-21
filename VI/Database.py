c82641efc95f94053940b17f19ddb95f3f40a872
Niks Šķērsts <niks_skersts@tutanota.com>
2021-12-22 00:43:12 +0200

    Finished 1

--- VI/Database.py
+++ VI/Database.py
@@ -1,28 +1,38 @@
 import sqlite3
+import time
+from datetime import datetime, timedelta, tzinfo, timezone
 from sqlite3 import Error
-from datetime import datetime
 
-conn = sqlite3.connect("data.db")
+import pytz as pytz
+
 
 def insert(code):
+    conn = sqlite3.connect("data.db")
     insert_query = ''' INSERT INTO barcode(code,date) VALUES (?,?)'''
     cur = conn.cursor()
     try:
-        cur.execute(insert_query, (code, str(datetime.now().time())))
+        current = datetime.now(pytz.timezone("Europe/Riga"))
+        ti = time.mktime(current.timetuple())
+        cur.execute(insert_query, (code, ti))
         conn.commit()
     except Error as e:
-        return 0
+        return -1
     return cur.lastrowid
 
 
 def count():
+    conn = sqlite3.connect("data.db")
     data_copy = conn.execute("SELECT max(rowid) from barcode")
     values = data_copy.fetchone()[0]
     return values
 
+
 def inquiry():
+    conn = sqlite3.connect("data.db")
+    codes_in_database = set(())
     cur = conn.cursor()
-    cur.execute("SELECT * FROM barcode")
+    cur.execute("SELECT code, date FROM barcode")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        codes_in_database.add(row)
+    return codes_in_database
--- VI/main.py
+++ VI/main.py
@@ -1,19 +1,100 @@
-# import the necessary packages
-from __future__ import print_function
-from GUI import QRDetector
-from imutils.video import VideoStream
-import argparse
+import sys
+from datetime import datetime
+import threading
 import time
+import cv2
+import Database
+from PIL import Image, ImageTk
+import tkinter as tk
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--picamera", type=int, default=-1,
-                help="whether or not the Raspberry Pi camera should be used")
-args = vars(ap.parse_args())
-# initialize the video stream and allow the camera sensor to warmup
-print("[INFO] warming up camera...")
-vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
-time.sleep(2.0)
-# start the app
-pba = QRDetector(vs)
-pba.root.mainloop()
+
+def popup():
+    result = Database.inquiry()
+    if result is not None:
+        popupRoot = tk.Tk()
+        tk.Label(popupRoot, text="Datums un laiks", font=('Mistral 18 bold')).pack()
+        for i in result:
+            dt = datetime.fromtimestamp(i[1]).strftime('%Y-%m-%d %H:%M:%S')
+            tk.Label(popupRoot, text="{0}, {1}".format(i[0], dt), font=('Mistral 18 bold')).pack()
+        popupRoot.mainloop()
+
+
+def ok_popup(code):
+    popupRoot = tk.Tk()
+    tk.Label(popupRoot, text="Atrastais QR : {0}".format(code)).pack()
+    tk.Button(popupRoot, text="OK!", font=("Verdana", 18), bg="yellow", command=popupRoot.destroy).pack()
+    return popupRoot
+
+
+def onClose():
+    print("[INFO] closing...")
+    stopEvent.set()
+    cap.release()
+    root.quit()
+    sys.exit(0)
+
+
+def video_loop():
+    global panel
+    try:
+        while not stopEvent.is_set():
+            _, frame = cap.read()
+            frame = read_barcodes(frame)
+            frame = cv2.flip(frame, 1)
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image=img)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("s"):
+                onClose()
+            if panel is None:
+                panel = tk.Label(image=imgtk)
+                panel.image = imgtk
+                panel.pack(side="left", padx=10, pady=10)
+            else:
+                panel.configure(image=imgtk)
+                panel.image = imgtk
+    except RuntimeError as e:
+        print("[INFO] caught a RuntimeError")
+
+
+def read_barcodes(frame):
+    data, points, _ = decoder.detectAndDecode(frame)
+    if points is not None:
+        points = points[0]
+        for i in range(len(points)):
+            pt1 = [int(val) for val in points[i]]
+            pt2 = [int(val) for val in points[(i + 1) % 4]]
+            cv2.line(frame, pt1, pt2, color=(255, 0, 0), thickness=3)
+        print(data)
+        if data != '' or "":
+            Database.insert(data)
+            ok = ok_popup(data)
+            ok.mainloop()
+    return frame
+
+
+stopEvent = threading.Event()
+panel = None
+decoder = cv2.QRCodeDetector()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("SCANNER")
+    root.config(background='black')
+    panel = tk.Label(root)
+    panel.grid(row=0, column=0)
+
+    cap = cv2.VideoCapture(0)
+    time.sleep(2.0)
+    detector = cv2.QRCodeDetector()
+    thread = threading.Thread(target=video_loop, args=())
+    thread.start()
+    # set a callback to handle when the window is closed
+    root.wm_title("qr")
+    root.wm_protocol("WM_DELETE_WINDOW", onClose)
+    popupButton = tk.Button(root, text="Izvadīt esošos QR", font=("Verdana", 12), bg="yellow", command=popup)
+    popupButton.grid(row=1, column=0)
+    root.mainloop()
+
